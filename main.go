ecda2b96a04511a8ab49b8c31cad7447e579ed1a
luoxiaojun <luoxiaojun@dodoca.com>
2018-09-19 15:39:06 +0800

    feat: init commit

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"os/exec"
+	"strings"
+	"fmt"
+	"sync"
+	"github.com/panjf2000/ants"
+	"log"
+	"flag"
+)
+
+var poolSize int
+
+func init() {
+	flag.IntVar(&poolSize, "pool", 20000, "Pool Size")
+	flag.Parse()
+}
+
+func main()  {
+	pool, err := ants.NewPool(poolSize)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pool.Release()
+
+	var wg sync.WaitGroup
+	for i := 0; i< 256; i++ {
+		for j := 0; j < 256; j++ {
+			ip := "192.168." + fmt.Sprintf("%d", i) + "." + fmt.Sprintf("%d", j)
+			wg.Add(1)
+			pool.Submit(func() error {
+				cmd := exec.Command("ping", ip, "-c", "1", "-W", "5")
+				output, err := cmd.Output()
+				if err != nil {
+					fmt.Println(ip + "Blocked")
+				} else {
+					if strings.Contains(string(output), "100.0% packet loss") {
+						fmt.Println(ip + "Blocked")
+					} else {
+						fmt.Println(ip + "Live")
+					}
+				}
+				wg.Done()
+				return nil
+			})
+		}
+	}
+	wg.Wait()
+
+	var wg2 sync.WaitGroup
+	for i := 0; i< 256; i++ {
+		for j := 0; j < 256; j++ {
+			for k := 0; k < 256; k++ {
+				ip := "10." + fmt.Sprintf("%d", i) + "." + fmt.Sprintf("%d", j) + "." + fmt.Sprintf("%d", k)
+				wg2.Add(1)
+				pool.Submit(func() error {
+					cmd := exec.Command("ping", ip, "-c", "1", "-W", "5")
+					output, err := cmd.Output()
+					if err != nil {
+						fmt.Println(ip + "Blocked")
+					} else {
+						if strings.Contains(string(output), "100.0% packet loss") {
+							fmt.Println(ip + "Blocked")
+						} else {
+							fmt.Println(ip + "Live")
+						}
+					}
+					wg2.Done()
+					return nil
+				})
+			}
+		}
+	}
+	wg2.Wait()
+}

77f35ced9ba82238d6763e8d5c12d3ace65fdc2b
luoxiaojun <luoxiaojun@dodoca.com>
2018-09-19 16:51:32 +0800

    refactor: reduce epoch

--- main.go
+++ main.go
@@ -17,6 +17,20 @@ func init() {
 	flag.Parse()
 }
 
+func ping(ip string) {
+	cmd := exec.Command("ping", ip, "-c", "1", "-W", "5")
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println(ip + "Blocked")
+	} else {
+		if strings.Contains(string(output), "100.0% packet loss") {
+			fmt.Println(ip + "Blocked")
+		} else {
+			fmt.Println(ip + "Live")
+		}
+	}
+}
+
 func main()  {
 	pool, err := ants.NewPool(poolSize)
 	if err != nil {
@@ -31,36 +45,23 @@ func main()  {
 			ip := "192.168." + fmt.Sprintf("%d", i) + "." + fmt.Sprintf("%d", j)
 			wg.Add(1)
 			pool.Submit(func() error {
-				cmd := exec.Command("ping", ip, "-c", "1", "-W", "5")
-				output, err := cmd.Output()
-				if err != nil {
-					fmt.Println(ip + "Blocked")
-				} else {
-					if strings.Contains(string(output), "100.0% packet loss") {
-						fmt.Println(ip + "Blocked")
-					} else {
-						fmt.Println(ip + "Live")
-					}
-				}
+				ping(ip)
 				wg.Done()
 				return nil
 			})
 
-			for k := 0; k < 256; k++ {
-				ip := "10." + fmt.Sprintf("%d", i) + "." + fmt.Sprintf("%d", j) + "." + fmt.Sprintf("%d", k)
+			for k := 0; k < 256; k = k + 2 {
+				ip1 := "10." + fmt.Sprintf("%d", i) + "." + fmt.Sprintf("%d", j) + "." + fmt.Sprintf("%d", k)
 				wg.Add(1)
 				pool.Submit(func() error {
-					cmd := exec.Command("ping", ip, "-c", "1", "-W", "5")
-					output, err := cmd.Output()
-					if err != nil {
-						fmt.Println(ip + "Blocked")
-					} else {
-						if strings.Contains(string(output), "100.0% packet loss") {
-							fmt.Println(ip + "Blocked")
-						} else {
-							fmt.Println(ip + "Live")
-						}
-					}
+					ping(ip1)
+					wg.Done()
+					return nil
+				})
+				ip2 := "10." + fmt.Sprintf("%d", i) + "." + fmt.Sprintf("%d", j) + "." + fmt.Sprintf("%d", k + 1)
+				wg.Add(1)
+				pool.Submit(func() error {
+					ping(ip2)
 					wg.Done()
 					return nil
 				})
